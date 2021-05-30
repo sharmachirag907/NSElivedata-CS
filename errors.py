a19364e9d3576632ff6d709b65d86caffca107a0
sharmachirag907 <69902742+sharmachirag907@users.noreply.github.com>
2021-05-30 15:34:10 +0530

    Delete errors.py

--- errors.py
+++
@@ -1,8 +0,0 @@
-class ChiragcopyNotAvailableError(Exception):
-    """this error could occur in case you download Chiragcopy for the dates
-    when the market was close"""
-    pass
-
-class DateFormatError(Exception):
-    """in case the date format is errorneous"""
-    pass
